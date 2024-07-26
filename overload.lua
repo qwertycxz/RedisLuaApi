b0773530fd21617f9eb0468b629c369e0e9199dd
qwertycxz <gmail2@qq.com>
2024-07-26 21:51:32 +0800

    Try to load the resp file

--- overload.lua
+++ overload.lua
@@ -1,2 +1,16 @@
-local cjson = require'cjson'
-print(cjson.null)
+local decode = require'cjson'.decode
+io.input('../data/resp2_replies.json')
+local resp2 = {}
+for k, v in pairs(decode(io.read('a'))) do
+	for i, w in ipairs(v) do
+		local match = w:match('%[.*%]%(')
+		if match then
+			if resp2[match] then
+				table.insert(resp2[match], k)
+			else
+				resp2[match] = {k}
+			end
+		end
+	end
+end
+print(resp2)
